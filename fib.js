0da4fcda0c87b30230dd970f1a3d6d3c5a7ea9f8
naimdasb <naimdasb@gmail.com>
2021-01-10 14:04:58 -0300

    tab fib

--- fib.js
+++ fib.js
@@ -1,6 +1,11 @@
 const { performance } = require('perf_hooks')
 
 function fib(n, memo = []) {
+    if (n <= 2) return 1
+    return fib(n - 1, memo) + fib(n - 2, memo);
+}
+
+function fibMemo(n, memo = []) {
     if (memo[n]) return memo[n]
     if (n <= 2) return 1
 
@@ -9,7 +14,30 @@ function fib(n, memo = []) {
     return memo[n];
 }
 
-let start = performance.now()
-console.log(fib(500));
+function fibTab(n) {
+    let numbers = Array(n + 1).fill(0);
+    numbers[1] = 1;
+
+    for (let i = 0; i <= n; i++) {
+        numbers[i + 1] += numbers[i]
+        numbers[i + 2] += numbers[i]
+    }
+
+    return numbers[n]
+}
+
+/* let start = performance.now()
+console.log(fib(5));
 let end = performance.now()
 console.log(end - start)
+
+let startMemo = performance.now()
+console.log(fibMemo(5));
+let endMemo = performance.now()
+console.log(endMemo - startMemo) */
+
+let startTab = performance.now()
+console.log(fibTab(50))
+let endTab = performance.now()
+console.log(endTab - startTab)
+
